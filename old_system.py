5ef6b86e4d34a5133751d11e31780e945f855213
robadonia <rd526@kent.ac.uk>
2026-02-09 15:53:00 +0000

    Ensure that the crew is removed when the name is not found
    Add error handling for name removal in old_system.py

--- old_system.py
+++ old_system.py
@@ -45,12 +45,14 @@ def run_system_monolith():
             
         elif opt == "3":
             rem = input("Name to remove: ")
-           
+        if rem in n: 
             idx = n.index(rem)
             n.pop(idx)
             r.pop(idx)
             d.pop(idx)
             print("Removed.")
+        else:
+            print("Name not found.")
             
         elif opt == "4":
             print("Analyzing...")
